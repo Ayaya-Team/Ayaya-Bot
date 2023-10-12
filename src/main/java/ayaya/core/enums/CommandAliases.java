1c1611b5a2c683fe3db86a52b4fdef3f26c4fca7
Aya-Komichi <ayaya.mosaic@gmail.com>
2023-10-12 16:16:06 +0100

    Deleted the commands Premium and PrivacyPolicy since they are no longer useful or relevant.

--- src/main/java/ayaya/core/enums/CommandAliases.java
+++ src/main/java/ayaya/core/enums/CommandAliases.java
@@ -20,11 +20,11 @@ public enum CommandAliases {
     DIE("die", new Roll()), EMOJITEXT("emojitext", new Bigtext()), FLIP("flip", new Flipcoin()),
     GUILDINFO("guildinfo", new Serverinfo()), HAPPY("happy", new Smile()), HI("hi", new Greet()),
     INFO("info", new About()), KONNICHIWA("konnichiwa", new Greet()), NANI("nani", new Confused()),
-    PFP("pfp", new Avatar()), PP("pp", new PrivacyPolicy()), PRIVACY("privacy", new PrivacyPolicy()),
-    PURGE("purge", new Prune()), REGIONALTEXT("regionaltext", new Bigtext()),
-    SHRUGS("shrugs", new Shrug()), STATISTICS("statistics", new Stats()),
-    TABLEFLIP("tableflip", new Fliptable()), TSUN("tsun", new Tsundere()),
-    UNFLIP("unflip", new Unfliptable()), WAA("waa", new Cry()), WHAT("what", new Confused());
+    PFP("pfp", new Avatar()), PURGE("purge", new Prune()),
+    REGIONALTEXT("regionaltext", new Bigtext()), SHRUGS("shrugs", new Shrug()),
+    STATISTICS("statistics", new Stats()), TABLEFLIP("tableflip", new Fliptable()),
+    TSUN("tsun", new Tsundere()), UNFLIP("unflip", new Unfliptable()), WAA("waa", new Cry()),
+    WHAT("what", new Confused());
 
     private String name;
     private Command cmd;
